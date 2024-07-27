68386e4604cc2c3c40f3691389896564eea8c252
aguascesar <cesarantonioaguas@gmail.com>
2024-07-27 15:13:34 -0400

    2024-FSP_M3d7-SentCondItera-DF-Sentencias1-byCesarAguas-v1

---
+++ cachipun.py
@@ -0,0 +1,41 @@
+# * my_cachipun v1 (https://github.com/...//)
+# * Copyright 2024 Cesar Aguas
+# * Licensed under AGPL3.0 (https://github.com/.../LICENSE)
+#   
+#
+
+import random
+import sys
+# Declaracion de funcion para jugar
+def juego(user_op):
+    alternativas = ['piedra', 'papel', 'tijera']
+    
+    if user_op not in alternativas:
+        print("Argumento inválido: Debe ser piedra, papel o tijera.")
+        return
+
+    pc_op = random.choice(alternativas)
+    
+    print("Tu jugaste:", user_op.capitalize())
+    print("Computador jugó:", pc_op.capitalize())
+
+    if user_op == pc_op:
+        print("Empate!")
+    elif (user_op == 'piedra' and pc_op == 'tijera') or (user_op == 'papel' and pc_op == 'piedra') or (user_op == 'tijera' and pc_op == 'papel'):
+        print("Ganaste!!")
+    else:
+        print("Perdiste!")
+
+
+# Se necesitan algunas comprobaciones
+
+# Comprobar cantidad de argumentos minimos
+if len(sys.argv) != 2:
+    print("Por favor escriba: python cachipun.py [piedra|papel|tijera]")
+    sys.exit(1)
+
+# Obtener alternativa del usuario, con todos los caracteres en minusculas
+user_op = sys.argv[1].lower()
+
+# Realizar jugada
+juego(user_op)
---
+++ imc.py
@@ -0,0 +1,60 @@
+# * my_imc v1 (https://github.com/...//)
+# * Copyright 2024 Cesar Aguas
+# * Licensed under AGPL3.0 (https://github.com/.../LICENSE)
+#   
+#
+import sys 
+#   Se intenta calcular el indice de masa muscular, mediante una funcion,
+#utilizando datos obtenidos mediante argumentos peso (k) y altura (cm)
+def indice(peso, altura):
+    imc = peso / (altura ** 2)
+    return round(imc, 2)
+
+#  Se intenta definir los grados de obesidad.
+#
+#       IMC     Clasificación OMS
+#       < 18.5     Bajo Peso
+#       18.5, 25  Adecuado
+#       25, 30    Sobrepeso
+#       30, 35    Obesidad Grado I
+#       35, 40    Obesidad Grado II
+#       > 40      Obesidad Grado III
+
+def escala(imc):
+
+    if imc < 18.5:
+        return "Bajo Peso"
+    elif 18.5 <= imc < 25:
+        return "Adecuado"
+    elif 25 <= imc < 30:
+        return "Sobrepeso"
+    elif 30 <= imc < 35:
+        return "Obesidad Grado I"
+    elif 35 <= imc < 40:
+        return "Obesidad Grado II"
+    else:
+        return "Obesidad Grado III"
+
+# Comenzando las rutinas
+if __name__ == "__main__":
+    # La cantidad de argumentos debe ser 2, = nombre_binario argumento_1 argumento_2
+    if len(sys.argv) != 3:
+        print("Por favor escriba: ./imc [kg] [cm]")
+        sys.exit(1)
+
+    try:
+        # Extraer argumentos
+        peso = float(sys.argv[1])
+        altura = float(sys.argv[2]) / 100
+    except Exception as e:
+        print("Error: ", e.message, e.args)
+        sys.exit(1)
+
+    # Realizar calculos
+    imc = indice(peso, altura)
+    oms = escala(imc)
+
+    print("Su IMC es:", imc)
+    print("La escala OMS es:", oms)
+
+
